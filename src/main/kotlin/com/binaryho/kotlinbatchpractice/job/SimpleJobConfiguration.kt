613d680030938d8810d5c1f5ba3ac2097515743f
binary-ho <dfghcvb11@naver.com>
2024-09-15 22:15:41 +0900

    Mysql 스키마 세팅, jobParameter 세팅

--- src/main/kotlin/com/binaryho/kotlinbatchpractice/job/SimpleJobConfiguration.kt
+++ src/main/kotlin/com/binaryho/kotlinbatchpractice/job/SimpleJobConfiguration.kt
@@ -2,10 +2,12 @@ package com.binaryho.kotlinbatchpractice.job
 
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
@@ -18,15 +20,19 @@ class SimpleJobConfiguration(
     @Bean
     fun simpleJob(): Job =
         JobBuilder("simple-job", jobRepository)
-            .start(simpleStep1())
+            .start(simpleStep1(null))
             .build()
 
     @Bean
-    fun simpleStep1(): Step =
+    @JobScope
+    fun simpleStep1(
+        @Value("#{jobParameters['requestDate']}") requestDate: String?,
+    ): Step =
         StepBuilder("simple-step-1", jobRepository)
             .tasklet(
                 { _, _ ->
                     println(">>>>>> Simple Step 1")
+                    println(">>>>>> requestDate = {$requestDate}")
                     RepeatStatus.FINISHED
                 },
                 transactionManager,
